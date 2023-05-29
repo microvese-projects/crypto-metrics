f1afe7f08da8a93f84969d53365a6fa6088a2652
Benson Njuguna <njugunab655@gmail.com>
2023-05-29 18:47:26 +0300

    Fetch data and set global state

--- src/Redux/Currencies/currenciesSlice.js
+++ src/Redux/Currencies/currenciesSlice.js
@@ -1,13 +1,51 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   Data: [],
+  loading: false,
+  isFetched: false,
+  Error: null,
 };
+
+export const fetchCurrencies = createAsyncThunk('currencies/fetchCurrencies', async () => {
+  try {
+    const URL = 'https://rest.coinapi.io/v1/assets';
+
+    const config = {
+      headers: { 'X-CoinAPI-Key': 'E745E079-5EE8-4926-85BC-9FC113C38AF4' },
+    };
+
+    const res = await axios.get(URL, config);
+    return res.data;
+  } catch (err) {
+    return err.message;
+  }
+});
 
 const currenciesSlice = createSlice({
   name: 'currencies',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCurrencies.fulfilled, (state, action) => {
+        const required = ['BTC', 'ETH', 'USDT', 'LTC', 'DOGE', 'TES'];
+        const fetched = action.payload;
+        const filtered = fetched.filter((each) => required.includes(each.asset_id));
+        state.Data = filtered;
+        state.loading = false;
+        state.Error = null;
+        state.isFetched = true;
+      })
+      .addCase(fetchCurrencies.rejected, (state, action) => {
+        state.Error = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchCurrencies.pending, (state) => {
+        state.loading = true;
+      });
+  },
 });
 
 export default currenciesSlice.reducer;
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCurrencies } from '../Redux/Currencies/currenciesSlice';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { isFetched } = useSelector((state) => state.currencies);
+  useEffect(() => {
+    if (!isFetched) {
+      dispatch(fetchCurrencies());
+    }
+  }, []);
+  return (
+    <div>Home</div>
+  );
+};
+
+export default Home;

5f1296a09440ff1ed0c12435d777c59034137d14
Benson Njuguna <njugunab655@gmail.com>
2023-05-29 18:48:12 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Details from './components/Details';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="details" element={<Details />} />
+      </Routes>
     </div>
   );
 }

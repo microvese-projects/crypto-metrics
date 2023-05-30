5503f85d152fd4e5a0eae39cd2c787a4fbc50ceb
Benson Njuguna <njugunab655@gmail.com>
2023-05-30 08:45:56 +0300

    Dispatch fetchrates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,31 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCurrencies } from '../Redux/Currencies/currenciesSlice';
+import { fetchRates } from '../Redux/Currencies/currencyRatesSlice';
+import HomeCard from './HomeCard';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { isFetched } = useSelector((state) => state.currencies);
+  const { isFetched, Data } = useSelector((state) => state.currencies);
   useEffect(() => {
     if (!isFetched) {
       dispatch(fetchCurrencies());
     }
   }, [dispatch, isFetched]);
+
+  const { dataFetched } = useSelector((state) => state.rates);
+
+  useEffect(() => {
+    if (!dataFetched) {
+      dispatch(fetchRates());
+    }
+  }, [dispatch, dataFetched]);
+
   return (
-    <div>Home</div>
+    <div>
+      Home page
+      <HomeCard name="Crypto" data={Data} />
+    </div>
   );
 };
 

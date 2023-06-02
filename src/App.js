9416221964e09d33230dc953a269db5a9e0cc9d2
Benson Njuguna <njugunab655@gmail.com>
2023-06-02 13:01:23 +0300

    Add converter feature

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Details from './components/Details';
+import Converter from './components/Converter';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="details" element={<Details />} />
+        <Route path="/details/converter" element={<Converter />} />
       </Routes>
     </div>
   );
